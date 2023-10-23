f64f2539be0ea10a7cffad336c986cc723427c42
Max Hargreave <max@monsun.dk>
2023-10-23 03:38:00 +0200

    nav a to Link

--- src/app/components/sidebar-nav.js
+++ src/app/components/sidebar-nav.js
@@ -1,23 +1,24 @@
+import Link from "next/link"
 
 export default function SidebarNav() {
         
     return (
         <div className="mb-8">
             <ul className="flex flex-col text-sm ">
-                <li><a href="vision" className="flex py-1.5" >Vision</a></li>
-                <li><a href="organisation" className="flex py-1.5" >Organisation</a></li>
-                <li><a href="historie" className="flex py-1.5" >Historie</a>
+                <li><Link href="/about/vision" className="flex py-1.5" >Vision</Link></li>
+                <li><Link href="/about/organisation" className="flex py-1.5" >Organisation</Link></li>
+                <li><Link href="/about/historie" className="flex py-1.5" >Historie</Link>
                     
                 </li>
-                <li><a href="internationalt" className="flex py-1.5" >Internationalt</a></li>
-                <li><a href="dokumenter" className="flex py-1.5" >Formelle dokumenter</a>
+                <li><Link href="/about/internationalt" className="flex py-1.5" >Internationalt</Link></li>
+                <li><Link href="/about/dokumenter" className="flex py-1.5" >Formelle dokumenter</Link>
                     <ul className="ml-2 pl-4 border-l border-stone-200 border-dotted my-1.5">
-                        <li><a href="/" className="flex pb-1.5" >Black tea</a></li>
-                        <li><a href="/" className="flex py-1.5" >Green tea</a></li>
-                        <li><a href="/" className="flex pt-1.5" >Green tea</a></li>
+                        <li><Link href="/" className="flex pb-1.5" >Black tea</Link></li>
+                        <li><Link href="/" className="flex py-1.5" >Green tea</Link></li>
+                        <li><Link href="/" className="flex pt-1.5" >Green tea</Link></li>
                     </ul>
                 </li>
-                <li><a href="netværk" className="flex py-1.5" >Netværk</a></li>
+                <li><Link href="/about/netværk" className="flex py-1.5" >Netværk</Link></li>
             </ul>
         </div>
     )
